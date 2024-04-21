be183a2990e8ef2f89ca24bdfeb7931ba9394ec0
Alex Dew <sabackup001@gmail.com>
2024-04-21 17:49:18 +0200

    Initial Commit

---
+++ LittleLemon - dinner menu - SA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LittleLemon - dinner menu - SA
+//
+//  Created by Aleksandr Surkov on 21/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LittleLemon - dinner menu - SA/LittleLemon___dinner_menu___SAApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemon___dinner_menu___SAApp.swift
+//  LittleLemon - dinner menu - SA
+//
+//  Created by Aleksandr Surkov on 21/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemon___dinner_menu___SAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
